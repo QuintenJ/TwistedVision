a238525412d5145e336bbc316e355b98827f3075
Quinten <Contact@quintenjames.com>
2019-02-04 13:02:13 -0700

    Player Input
    Handle keyboard input and allow moving the Player around the screen. Update the field of view as the player moves. Ensures walls cannot be walked through.

---
+++ Core/Direction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwistedVision.Core;
+
+namespace TwistedVision.Core
+{
+    public enum Direction
+    {
+        None = 0,
+        DownLeft = 1,
+        Down = 2,
+        DownRight = 3,
+        Left = 4,
+        Center = 5,
+        Right = 6, 
+        Upleft = 7,
+        Up = 8,
+        UpRight = 9
+    }
+}
--- Game.cs
+++ Game.cs
@@ -10,11 +10,9 @@ using TwistedVision.Systems;
 
 namespace TwistedVision
 {
-    class Game
+    public static class Game
     {
-        public static Player Player { get; private set; }
-        public static DungeonMap DungeonMap { get; private set; }
-        // The screen height and wij./  dth are in number of tiles
+        // The screen height and width are in number of tiles
         private static readonly int _screenWidth = 100;
         private static readonly int _screenHeight = 70;
         private static RLRootConsole _rootConsole;
@@ -39,40 +37,43 @@ namespace TwistedVision
         private static readonly int _inventoryHeight = 11;
         private static RLConsole _inventoryConsole;
 
+        private static bool _renderRequired = true;
 
-        static void Main()
+        public static Player Player { get; private set; }
+        public static DungeonMap DungeonMap { get; private set; }
+        public static CommandSystem CommandSystem { get; private set; }
+
+        public static void Main()
         {
+            // This must be the exact name of the bitmap font file we are using or it will error.
+            string fontFileName = "terminal8x8.png";
+
+            // The title will appear at the top of the console window
+            string consoleTitle = "RougeSharp V3 Tutorial - Level 1";
+
+            // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+
+            // Initialize the sub consoles that we will Blit to the root console
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
             Player = new Player();
             MapGenerator mapGenerator = new MapGenerator(_mapWidth, _mapHeight);
             DungeonMap = mapGenerator.CreateMap();
             DungeonMap.UpdatePlayerFieldOfView();
 
-            // This must be the exact name of the bitmap font file we are using or it will error
-            string fontFileName = "terminal8x8.png";
-            // The title will apear at the top of the console window
-            string consoleTitle = "Twisted Vision - Level 1";
-            //Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
-            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
-            // Initialize the sub console that we will Blit to the root console
-            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
-            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
-            _statConsole = new RLConsole(_statWidth, _statHeight);
-            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+            CommandSystem = new CommandSystem();
+
             // Set up a handler for RLNET's Update event
             _rootConsole.Update += OnRootConsoleUpdate;
+
             // Set up a handler for RLNET's Render event
             _rootConsole.Render += OnRootConsoleRender;
-            // Begin RLNET's game loop
-            _rootConsole.Run();
-        }
-        // Event handler for RLNET's Update event
-        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
-        {
-            //Set background color and text for each console
-            // so that we can verify they are in the correct positons
-            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorBackground);
-            _mapConsole.Print(1, 1, "Map", Colors.TextHeading);
 
+            // Set background color and text for each console so that we can verify they are in the correct positions
             _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Palette.DbDeepWater);
             _messageConsole.Print(1, 1, "Messages", Colors.TextHeading);
 
@@ -81,26 +82,67 @@ namespace TwistedVision
 
             _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Palette.DbWood);
             _inventoryConsole.Print(1, 1, "Inventory", Colors.TextHeading);
+
+            // Begin RLNET's game loop
+            _rootConsole.Run();
         }
+
+        // Event handler for RLNET's Update event
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            bool didPlayerAct = false;
+            RLKeyPress keyPress = _rootConsole.Keyboard.GetKeyPress();
+
+            if (keyPress != null)
+            {
+                if (keyPress.Key == RLKey.Up)
+                {
+                    didPlayerAct = CommandSystem.MovePlayer(Direction.Up);
+                }
+                else if (keyPress.Key == RLKey.Down)
+                {
+                    didPlayerAct = CommandSystem.MovePlayer(Direction.Down);
+                }
+                else if (keyPress.Key == RLKey.Left)
+                {
+                    didPlayerAct = CommandSystem.MovePlayer(Direction.Left);
+                }
+                else if (keyPress.Key == RLKey.Right)
+                {
+                    didPlayerAct = CommandSystem.MovePlayer(Direction.Right);
+                }
+                else if (keyPress.Key == RLKey.Escape)
+                {
+                    _rootConsole.Close();
+                }
+            }
+
+            if (didPlayerAct)
+            {
+                _renderRequired = true;
+            }
+        }
+
         // Event handler for RLNET's Render event
         private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
         {
-            
-            DungeonMap.Draw(_mapConsole);
-            Player.Draw(_mapConsole, DungeonMap);
-            // Blit the sub consoles to the root console in the correct locations
-            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
-              _rootConsole, 0, _inventoryHeight);
-            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight,
-              _rootConsole, _mapWidth, 0);
-            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
-              _rootConsole, 0, _screenHeight - _messageHeight);
-            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight,
-              _rootConsole, 0, 0);
-            
-            // Tell RLNET to draw the console that we set
-            _rootConsole.Draw();
+            // Don't bother redrawing all of the consoles if nothing has changed.
+            if (_renderRequired)
+            {
+                DungeonMap.Draw(_mapConsole);
+                Player.Draw(_mapConsole, DungeonMap);
+
+                // Blit the sub consoles to the root console in the correct locations
+                RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+                RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+                RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+                RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+
+                // Tell RLNET to draw the console that we set
+                _rootConsole.Draw();
+
+                _renderRequired = false;
+            }
         }
     }
-    
 }
